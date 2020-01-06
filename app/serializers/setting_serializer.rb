d9a764743e28059fc08597579413d4632b2a47c4
Andres Felipe Vera Arenas <andoresu95@gmail.com>
2020-01-06 17:11:14 -0500

    fix market cap

--- app/serializers/setting_serializer.rb
+++ app/serializers/setting_serializer.rb
@@ -1,11 +1,11 @@
 class SettingSerializer < ApplicationSerializer
-    
+
     include CurrencyHelper
-    
+
     attributes :last_trade_price, :purchase_percentage, :sale_percentage, :hour_volume, :active_traders, :market_cap, :daily_transactions, :active_accounts, :supported_countries
-    
+
     has_one :country
-  
+
     def last_trade_price
         return pretty? ? "#{string_to_money(object.last_trade_price, 0)} USD" : object.last_trade_price
     end
@@ -22,7 +22,7 @@ class SettingSerializer < ApplicationSerializer
         return pretty? ? "#{object.active_traders}" : object.active_traders
     end
     def market_cap
-        return pretty? ? "#{string_to_money(object.market_cap, 0)}" : object.market_cap
+        object.market_cap
     end
     def daily_transactions
         return pretty? ? "#{object.daily_transactions}" : object.daily_transactions
@@ -33,7 +33,7 @@ class SettingSerializer < ApplicationSerializer
     def supported_countries
         return pretty? ? "#{object.supported_countries}" : object.supported_countries
     end
-    
+
     def pretty?
         return scope == "pretty"
     end
